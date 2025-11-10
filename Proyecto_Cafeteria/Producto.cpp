6222fdfddde4637db53eed7cb434829713bfe828
Rodrigo Medina <118804738+RodriiMedina@users.noreply.github.com>
2025-11-10 18:29:17 -0300

    cambios

--- Proyecto_Cafeteria/Producto.cpp
+++ Proyecto_Cafeteria/Producto.cpp
@@ -1,6 +1,38 @@
 #include "Producto.h"
+#include <iostream>
+#include <cstring>
 
-Producto::Producto()
+Producto::Producto(int idProducto, const char* nombre, bool categoria, float precio, int stock)
 {
+    /*
+    this->setId(idProducto);
+    this->setCategoria(categoria);
+    this->setStock(stock);
+    */
 
+    this->setNombre(nombre);
+    this->setPrecio(precio);
+
+}
+
+
+void Producto::setPrecio(float precio){
+    if(precio>0.0){
+        this -> precio=precio;
+        }else{
+            std::cout<<"El precio no puede ser negativo"<<std::endl;
+        }
+}
+
+void Producto::setNombre(const char* nombre){
+    if(nombre!=nullptr && nombre[0]!= '\0'){
+        strncpy(this -> nombre, nombre, maxNombre-1);
+        this->nombre[maxNombre - 1] = '\0';
+    }else{ this->nombre[0]= '\0';
+        std::cout<<"el nombre ingresado está vacio o es invalido"<<std::endl;
+    }
+}
+
+bool Producto::setCategoria(bool categoria){
+    this->categoria=categoria;
 }
--- Proyecto_Cafeteria/Producto.h
+++ Proyecto_Cafeteria/Producto.h
@@ -4,9 +4,20 @@
 class Producto
 {
     public:
-        Producto();
+    Producto(int idProducto, const char* nombre, bool categoria, float precio, int stock);
+    void setPrecio(float precio);
+    void setNombre(const char* nombre);
+    bool setCategoria(bool categoria);
 
     protected:
 
     private:
+    static const int maxNombre=20;
+
+       const int idProducto=0;
+       char nombre[maxNombre]="0/";
+       bool categoria;
+       float precio=0.0;
+       int stock=0;
+
 };
